507866886fc2beaf63ca5d0edeab6548f895bc02
david <andrianarivo.david@gmail.com>
2023-10-23 18:08:59 +0300

    Update Devise for extra Username Signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  # devise authorise username field
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # devise authorise username field
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation])
+  end
 end
