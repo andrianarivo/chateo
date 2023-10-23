d22fde92fae5337ded220c4ff4ca7a2bd04e8bde
david <andrianarivo.david@gmail.com>
2023-10-23 18:09:54 +0300

    Channels initial Implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  resources :channels
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
+
+  get 'user/:id', to: 'users#show', as: 'user'
 end
